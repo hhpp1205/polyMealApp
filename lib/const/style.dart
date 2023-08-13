65c3235f4609059317ce64899181b64147f4bab7
hsm <hhpp1205@naver.com>
2023-08-13 13:48:32 +0900

    Feat : HOST, COLOR 상수 값 추가

--- lib/const/style.dart
+++ lib/const/style.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 //Color
 const COLOR_NAVY =  Color(0xff102C57);
 const COLOR_IVORY = Color(0xffF6F4EB);
+const COLOR_GRAY = Color(0xffD8D9DA);
+const COLOR_BLACK = Colors.black;
+const COLOR_WHITE = Colors.white;
+const COLOR_GREEN = Color(0xff1A5D1A);
 
 //TextStyle
-const TEXT_STYLE = TextStyle(color: COLOR_IVORY, fontWeight: FontWeight.w700,);
+const TEXT_STYLE = TextStyle(color: COLOR_BLACK, fontWeight: FontWeight.w700,);
